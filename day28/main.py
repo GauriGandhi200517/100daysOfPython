7d7b2aa35eed024bd19a26c67bc8b32d270c9d2f
Gauri gandhi <gauri.gandhi4827@gmail.com>
2025-03-24 01:13:30 +0530

    create day28

---
+++ day28/main.py
@@ -0,0 +1,10 @@
+letter = "Hey my name is {1} and I am form {0}"
+country="INDIA"
+name="GAURI"
+
+print(letter.format(country,name))
+print(f"Hey my name is {name} and I am form {country}")
+price=49.09999
+txt =f" for only {price:.2f} dollars"
+print(txt)
+print (type(f"{2*30})"))
